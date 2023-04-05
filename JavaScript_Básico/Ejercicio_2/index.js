52e632848fbad4345649d9f79f3ec438f548ad65
Mauro_Franco <49313625+Mauro11F@users.noreply.github.com>
2023-04-05 13:42:52 -0300

    terminado N 2
    Terminado el ejercicio N°2

---
+++ JavaScript_Básico/Ejercicio_2/index.js
@@ -0,0 +1,14 @@
+const datos = [
+   "Mauro", 
+   29, 
+   true, 
+   new Date(1993, 6, 13), 
+   {
+      titulo: "El Silmarillion",
+      autor: "J. R. R. Tolkien",
+      fecha: new Date(1984, 02 ,1),
+      url: "https://es.wikipedia.org/wiki/El_Silmarillion#Bibliograf%C3%ADa"
+   }
+];
+
+console.log(datos);
